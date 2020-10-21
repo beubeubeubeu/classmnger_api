9d9d6efd85944bad8c7a856eafb99329b8b6c7ac
beubeubeubeu <nguyen.hl.b@gmail.com>
2020-10-22 01:42:04 +0200

    Add user athent with devise_token_auth gem

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,17 @@ module ClassmngrApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Carefull with that one
+    # TODO only allow frontend calls
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: [:get, :post, :options, :delete, :put, :patch]
+      end
+    end
   end
 end
